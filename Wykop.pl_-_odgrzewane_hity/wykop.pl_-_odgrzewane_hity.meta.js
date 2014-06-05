c0839df97892af765ba7ae5ffb8c9abd96bbfb6f
Kulmegil <kulmegil@gmail.com>
2014-06-05 22:48:16 +0200

    Wykop.pl - odgrzewane hity - fix script url's (again)

--- Wykop.pl_-_odgrzewane_hity/wykop.pl_-_odgrzewane_hity.meta.js
+++ Wykop.pl_-_odgrzewane_hity/wykop.pl_-_odgrzewane_hity.meta.js
@@ -2,8 +2,9 @@
 // @name        Wykop.pl - odgrzewane hity
 // @namespace   odgrzewane_hity@wykop.pl
 // @description Dodaje nowe możliwości filtrowania Hitów - z ostatnich 48h lub wszystkie, a także Gorących wpisów - z ostatnich 1h, 2h, 3h, 48h, 7dni i 30 dni.
-// @icon        https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_odgrzewane_hityicon.png
+// @icon        https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_odgrzewane_hity/icon.png
 // @updateURL   https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_odgrzewane_hity/wykop.pl_-_odgrzewane_hity.meta.js
+// @downloadURL https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_odgrzewane_hity/wykop.pl_-_odgrzewane_hity.user.js
 // @include     http://www.wykop.pl/*
 // @version     1.0.0
 // @grant       none
