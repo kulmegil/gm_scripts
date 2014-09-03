7b1a047bb58dcd58fd51e169c015f716424b5f9e
kulmegil <Kulmegil@gmail.com>
2014-09-03 23:22:36 +0200

    description fix

--- Wykop.pl_-_SFW/wykop.pl_-_sfw.meta.js
+++ Wykop.pl_-_SFW/wykop.pl_-_sfw.meta.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name        Wykop.pl - SFW
 // @namespace   SFW@wykop.pl
-// @description Przycisk w menu pod awatarem pozwalający ukryć znaleziska oznaczone jako 18+ Automatycznie blokuje tag #nsfw i oraz wybrane tagi użytkownika.
+// @description Dodaje przycisk w menu pod awatarem pozwalający ukryć/pokazać znaleziska oznaczone jako 18+ Blokuje tag #nsfw oraz wybrane tagi użytkownika.
 // @include     http://www.wykop.pl/*
 // @version     1.1.0
 // @icon        https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_SFW/icon.png
