046eaf7951477a8238306b227781045d3e46bcf8
kulmegil <Kulmegil@gmail.com>
2014-09-03 22:59:02 +0200

    .

--- Wykop.pl_-_SFW/wykop.pl_-_sfw.meta.js
+++ Wykop.pl_-_SFW/wykop.pl_-_sfw.meta.js
@@ -4,8 +4,8 @@
 // @description Przycisk w menu pod awatarem pozwalający ukryć znaleziska oznaczone jako 18+ Automatycznie blokuje tag #nsfw i oraz wybrane tagi użytkownika.
 // @include     http://www.wykop.pl/*
 // @version     1.1.0
-// @icon        https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_sfw/icon.png
-// @updateURL   https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_sfw/wykop.pl_-_sfw.meta.js
-// @downloadURL https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_sfw/wykop.pl_-_sfw.user.js
+// @icon        https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_SFW/icon.png
+// @updateURL   https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_SFW/wykop.pl_-_sfw.meta.js
+// @downloadURL https://github.com/kulmegil/gm_scripts/raw/master/Wykop.pl_-_SFW/wykop.pl_-_sfw.user.js
 // @grant       none
 // ==/UserScript==
